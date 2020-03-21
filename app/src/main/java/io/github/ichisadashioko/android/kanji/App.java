8fbef9ca657da5324a3c363485ed248b3b5fb8b1
Ichisada Shioko <ichisadashioko@gmail.com>
2020-03-21 20:29:03 +0700

    :robot: clear canvas with button

--- app/src/main/java/io/github/ichisadashioko/android/kanji/App.java
+++ app/src/main/java/io/github/ichisadashioko/android/kanji/App.java
@@ -2,8 +2,14 @@ package io.github.ichisadashioko.android.kanji;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import io.github.ichisadashioko.android.kanji.views.HandwritingCanvas;
 
 public class App extends Activity {
+    private HandwritingCanvas canvas;
+    private Button clearButton;
 //    private KanjiClassifier classifier;
 
 //    private void initializeClassifier(Activity activity) {
@@ -21,6 +27,14 @@ public class App extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        canvas = (HandwritingCanvas) findViewById(R.id.canvas);
+        clearButton = (Button) findViewById(R.id.clear);
 //        initializeClassifier(this);
+    }
+
+    public void clearCanvas(View view) {
+        if (canvas != null) {
+            canvas.clearCanvas();
+        }
     }
 }
--- lf.sh
+++ lf.sh
@@ -1,1 +1,8 @@
-find . -type f -print0 | xargs -0 dos2unix
+find . -type f \
+    -not -path "./app/build/*" \
+    -not -path "./.git/*" \
+    -not -path "./captures/*" \
+    -not -path "./.idea/*" \
+    -not -path "./gradle/*" \
+    -not -path "./.gradle/*" \
+    -print0 | xargs -0 dos2unix
