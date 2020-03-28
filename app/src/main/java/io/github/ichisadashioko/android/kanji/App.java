635c86474b2ad95a582f44d7ca51de3ffca86312
Ichisada Shioko <ichisadashioko@gmail.com>
2020-03-28 13:08:17 +0700

    :lipstick: :fire: format code
    - organize UI
    - remove WRITE_EXTERNAL_STORAGE

--- app/src/main/java/io/github/ichisadashioko/android/kanji/App.java
+++ app/src/main/java/io/github/ichisadashioko/android/kanji/App.java
@@ -1,25 +1,19 @@
 package io.github.ichisadashioko.android.kanji;
 
-import android.Manifest;
 import android.app.Activity;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.os.Environment;
 import android.os.SystemClock;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ToggleButton;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
@@ -29,9 +23,8 @@ import io.github.ichisadashioko.android.kanji.views.TouchCallback;
 import io.github.ichisadashioko.android.kanji.views.HandwritingCanvas;
 import io.github.ichisadashioko.android.kanji.views.ResultButton;
 
-public class App extends Activity implements TouchCallback, ActivityCompat.OnRequestPermissionsResultCallback {
+public class App extends Activity implements TouchCallback {
     public static final String KANJI_FONT_PATH = "fonts/HGKyokashotai_Medium.ttf";
-    public static final int PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE = 0;
 
     private HandwritingCanvas canvas;
     private OldClassifier tflite;
@@ -93,7 +86,6 @@ public class App extends Activity implements TouchCallback, ActivityCompat.OnReq
             public void onClick(View v) {
                 textRenderer.setText(textRenderer.getText() + btn.label);
                 textRenderer.setSelection(textRenderer.getText().length());
-                saveEvaluatedData(r, image);
             }
         });
         return btn;
@@ -135,46 +127,6 @@ public class App extends Activity implements TouchCallback, ActivityCompat.OnReq
     public void onTouchEnd() {
         if (autoEvaluate) {
             runClassifier(null);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // permission was granted, yay! Do the task you need to do.
-                } else {
-                    // permission denied, boo! Disable the functionality that depends on this permission.
-                }
-                return;
-            }
-
-            // other 'case' lines to check for other permissions this app might request.
-        }
-    }
-
-    public void saveEvaluatedData(Recognition result, Bitmap image) {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            // Permission is not granted
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-            } else {
-                // No explanation needed; request the permission
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE);
-
-                // PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE is an app-defined int constant.
-                // The callback method gets the result of the request.
-            }
-        } else {
-            File downloadDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
-            String packageName = this.getPackageName();
-            String imageDirectoryName = "images";
-            System.out.println(downloadDirectory);
         }
     }
 }
