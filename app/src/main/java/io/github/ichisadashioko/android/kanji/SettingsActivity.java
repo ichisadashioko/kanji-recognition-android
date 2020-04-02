0cca7ff00d9a8ded07b6ec1f44ddda2a7b98e05b
Ichisada Shioko <ichisadashioko@gmail.com>
2020-04-02 17:49:39 +0700

    request WRITE_EXTERNAL_STORAGE permission

--- app/src/main/java/io/github/ichisadashioko/android/kanji/SettingsActivity.java
+++ app/src/main/java/io/github/ichisadashioko/android/kanji/SettingsActivity.java
@@ -1,22 +1,74 @@
 package io.github.ichisadashioko.android.kanji;
 
+import android.Manifest;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.preference.PreferenceManager;
 
-import io.github.ichisadashioko.android.kanji.views.SettingsFragment;
+public class SettingsActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
+    public static final int PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE_CODE = 0;
 
-public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // TODO this activity will crash if we change permission from the Settings app
+        // https://stackoverflow.com/a/56765912/8364403
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            setSaveWritingDataPreference(false);
+        }
 
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.settings_container, new SettingsFragment())
                 .commit();
+    }
+
+
+    private void setSaveWritingDataPreference(boolean value) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(getString(R.string.pref_key_save_data), value);
+        editor.apply();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE_CODE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the task you need to do.
+                    setSaveWritingDataPreference(true);
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    setSaveWritingDataPreference(false);
+                }
+
+                reload();
+
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+
+    private void reload() {
+        // https://stackoverflow.com/a/38720750/8364403
+        finish();
+        overridePendingTransition(0, 0);
+        startActivity(getIntent());
+        overridePendingTransition(0, 0);
     }
 
     public void closeSettings(View v) {
---
+++ app/src/main/java/io/github/ichisadashioko/android/kanji/SettingsFragment.java
@@ -0,0 +1,80 @@
+package io.github.ichisadashioko.android.kanji;
+
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.Lifecycle;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SwitchPreferenceCompat;
+
+import static io.github.ichisadashioko.android.kanji.SettingsActivity.PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE_CODE;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.preference, rootKey);
+        FragmentActivity thisActivity = getActivity();
+
+        SwitchPreferenceCompat saveDataPref = findPreference(getString(R.string.pref_key_save_data));
+
+        if (saveDataPref != null) {
+
+            saveDataPref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                /**
+                 * Listen to the changed value. If the value is set to `true`,
+                 * check `WRITE_EXTERNAL_STORAGE` permission and request it
+                 * if the permission is not granted.
+                 * @param preference
+                 * @param newValue
+                 * @return `true` if the permission is successfully changed.
+                 */
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    boolean saveDataPrefValue = (boolean) newValue;
+                    if (saveDataPrefValue) {
+                        // check and request `WRITE_EXTERNAL_STORAGE`
+
+                        // We can only request permission while in foreground.
+                        // Users can change permission from Settings while our app is paused.
+                        if (thisActivity.getLifecycle().getCurrentState().isAtLeast(Lifecycle.State.RESUMED)) {
+                            if (ContextCompat.checkSelfPermission(thisActivity, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                                // Permission is not granted
+                                // Should we show an explanation?
+                                if (ActivityCompat.shouldShowRequestPermissionRationale(thisActivity, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                                    // TODO
+                                    // Show an explanation to the user *asynchronously* -- don't block
+                                    // this thread waiting for the user's response! After the user
+                                    // sees the explanation, try again to request the permission.
+                                    ActivityCompat.requestPermissions(thisActivity, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE_CODE);
+                                } else {
+                                    // No explanation needed; request the permission
+                                    ActivityCompat.requestPermissions(thisActivity, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE_CODE);
+
+                                    // PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE_CODE is an app-defined
+                                    // int constant. The callback method gets the result of the request.
+                                }
+
+                                return false;
+                            } else {
+                                // Permission has already been granted
+                                return true;
+                            }
+                        } else {
+                            return true;
+                        }
+                    } else {
+                        // We only turn off the preference.
+                        return true;
+                    }
+                }
+            });
+        }
+    }
+}
